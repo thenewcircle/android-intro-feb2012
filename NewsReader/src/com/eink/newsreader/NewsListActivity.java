080b0ec4d23263be7b643076ade645e8a256c657
Marko Gargenta <marko@marakana.com>
2012-02-21 14:05:45 -0800

    Created new project

---
+++ NewsReader/src/com/eink/newsreader/NewsListActivity.java
@@ -0,0 +1,13 @@
+package com.eink.newsreader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewsListActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
