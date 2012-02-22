2c20393d51248df8a4282f2abaa5da0143f74a1c
Marko Gargenta <marko@marakana.com>
2012-02-22 10:59:39 -0800

    Implemented WebView for the feed output

--- NewsReader/src/com/eink/newsreader/NewsListActivity.java
+++ NewsReader/src/com/eink/newsreader/NewsListActivity.java
@@ -5,7 +5,7 @@ import java.util.List;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
+import android.webkit.WebView;
 import android.widget.Toast;
 
 import com.eink.parser.FeedParser;
@@ -14,12 +14,12 @@ import com.eink.parser.Post;
 
 public class NewsListActivity extends Activity {
 	static final String TAG = "NewsListActivity";
+	static final int MAX_LENGTH = 250;
 	String feedUrl = "http://marakana.com/s/feed.rss";
-	TextView textOutput;
+	WebView output;
 	FeedParser parser;
 	List<Post> posts;
 
-	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -27,21 +27,36 @@ public class NewsListActivity extends Activity {
 		setContentView(R.layout.main);
 
 		// Find views
-		textOutput = (TextView) findViewById(R.id.text_output);
+		output = (WebView) findViewById(R.id.output);
 
 		// Initialize parser
 		parser = FeedParserFactory.getParser(feedUrl);
 
 		try {
+			StringBuffer content = new StringBuffer();
+
 			// Get the posts
 			posts = parser.parse();
 
+			String desc;
 			// Iterate over posts
 			for (Post post : posts) {
-				textOutput.append(post.toString() + "\n\n");
+				desc = post.getDescription();
+				// ellipsis
+				if(desc.length()>MAX_LENGTH) {
+					desc = desc.substring(0, MAX_LENGTH-3)+"...";
+				}
+				// Create html output
+				content.append(String.format(
+						"<a href=%s><h2>%s</h2></a>\n%s\n<hr/>",
+						post.getLink(), post.getTitle(), desc));
 			}
+
+			// Update output
+			output.loadData(content.toString(), "text/html", "utf-8");
+
 		} catch (Exception e) {
-			String message = "Problems parsing the feed: "+feedUrl;
+			String message = "Problems parsing the feed: " + feedUrl;
 			Toast.makeText(this, message, Toast.LENGTH_LONG).show();
 			Log.e(TAG, message, e);
 		}
