a9aa4ecfc54a8cd33ad0803889b6e078b1bd32c1
Marko Gargenta <marko@marakana.com>
2012-02-22 09:47:04 -0800

    Outputing feed in a simple text view

--- NewsReader/src/com/eink/newsreader/NewsListActivity.java
+++ NewsReader/src/com/eink/newsreader/NewsListActivity.java
@@ -1,20 +1,39 @@
 package com.eink.newsreader;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.eink.parser.FeedParser;
+import com.eink.parser.FeedParserFactory;
+import com.eink.parser.Post;
+
 public class NewsListActivity extends Activity {
+	String feedUrl = "http://marakana.com/s/feed.rss";
 	TextView textOutput;
+	FeedParser parser;
+	List<Post> posts;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+     
+        // Find views
         textOutput = (TextView) findViewById(R.id.text_output);
         
-        textOutput.append("Hello!");
+        // Initialize parser
+        parser = FeedParserFactory.getParser(feedUrl);
+        
+        // Get the posts
+        posts = parser.parse();
+        
+        // Iterate over posts
+        for(Post post: posts) {
+        		textOutput.append( post.toString() + "\n\n" );
+        }
     }
 }
