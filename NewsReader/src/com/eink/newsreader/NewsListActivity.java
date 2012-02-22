413dcf6c75510d1bf175d0774bb19c9df2e4b0d6
Marko Gargenta <marko@marakana.com>
2012-02-22 08:22:27 -0800

    Implemented findViewById()

--- NewsReader/src/com/eink/newsreader/NewsListActivity.java
+++ NewsReader/src/com/eink/newsreader/NewsListActivity.java
@@ -2,12 +2,19 @@ package com.eink.newsreader;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class NewsListActivity extends Activity {
+	TextView textOutput;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        textOutput = (TextView) findViewById(R.id.text_output);
+        
+        textOutput.append("Hello!");
     }
 }
